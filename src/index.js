e9d99cdcfc3461cb07b3126f80efbb07ec298910
王勇 <wangyong@wangxiaoyongdeMacBook-Air.local>
2019-05-22 21:31:33 +0800

    管理资源

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 
 import _ from 'lodash';
+import './style.css';
+import Icon from './wz.png';
 
 function component() {
 	const element = document.createElement('div');
 
 	element.innerHTML = _.join(['hello', 'webpack'], ' ');
+	element.classList.add('hello')
+
+	//将图像添加到我们已经存在的div中
+	var myIcon = new Image();
+	myIcon.src = Icon;
+	element.appendChild(myIcon);
 
 	return element
 }
