6f1eccb02c7934e005521b000e0f1353e2dd1abb
王勇 <wangyong@wangxiaoyongdeMacBook-Air.local>
2019-05-22 20:31:18 +0800

    creating a bundle

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+
+import _ from 'lodash';
+
 function component() {
 	const element = document.createElement('div');
 
-	//loash,currently included via a script ,is required for this line to work;
 	element.innerHTML = _.join(['hello', 'webpack'], ' ');
 
 	return element
