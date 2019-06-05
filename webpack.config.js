666b78036efdc4a1c0b13d81fbbfec703fb1ad63
王勇 <wangyong@wangxiaoyongdeMacBook-Air.local>
2019-06-05 15:49:57 +0800

    caching

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,19 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 module.exports = {
 	entry: {
 		app: './src/index.js',
-		print: './src/print.js'
 	},
 	plugins:[
 		new CleanWebpackPlugin(),
 		new HtmlWebpackPlugin({
-			title: 'Output Management'
+			title: 'Caching'
 		})
 	],
 	output: {
-		filename: '[name].bundle.js',
+		filename: '[name].[hash].js',
 		path: path.resolve(__dirname, 'dist')
 	}
 }
